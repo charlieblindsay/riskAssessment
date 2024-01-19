fdd5b58702905915b1f0dfda35da21c7ea3818c9
Charlie Lindsay <charlieblindsay@gmail.com>
2024-01-19 20:37:17 +0000

    trying w/o eval

--- streamlit.py
+++ streamlit.py
@@ -118,89 +118,9 @@ class OpenAILLM(LLMCaller):
     def get_model_output(self, prompt_input: Type[PromptInput]):
         LLM_output = self.get_JSON_output_from_API_call(prompt_input)
         return LLM_output.choices[0].message["content"]
-
-try:
-    from RiskAssessment import RiskAssessment
-    # from LLMCaller import LLMWithCandidateLabels, LLMWithGeneratedText
-except:
-    from .RiskAssessment import RiskAssessment
-    # from .LLMCaller import LLMWithCandidateLabels, LLMWithGeneratedText, OpenAILLM
-
-class Params(TypedDict):
-    pass
-
-class Result(TypedDict):
-    is_correct: bool
-    feedback: str
-
-def evaluation_function(response: Any, answer: Any, params: Any) -> Result:
-    """
-    Function used to evaluate a student response.
-    ---
-    The handler function passes three arguments to evaluation_function():
-
-    - `response` which are the answers provided by the student.
-    - `answer` which are the correct answers to compare against.
-    - `params` which are any extra parameters that may be useful,
-        e.g., error tolerances.
-
-    The output of this function is what is returned as the API response
-    and therefore must be JSON-encodable. It must also conform to the
-    response schema.
-
-    Any standard python library may be used, as well as any package
-    available on pip (provided it is added to requirements.txt).
-
-    The way you wish to structure you code (all in this function, or
-    split into many) is entirely up to you. All that matters are the
-    return types and that evaluation_function() is the main function used
-    to output the evaluation response.
-    """
-
-    activity, hazard, who_it_harms, how_it_harms, uncontrolled_likelihood, uncontrolled_severity, uncontrolled_risk, prevention, mitigation, controlled_likelihood, controlled_severity, controlled_risk = np.array(response).flatten()
-
-    RA = RiskAssessment(activity=activity, hazard=hazard, who_it_harms=who_it_harms, how_it_harms=how_it_harms,
-                        uncontrolled_likelihood=uncontrolled_likelihood, uncontrolled_severity=uncontrolled_severity,
-                        uncontrolled_risk=uncontrolled_risk, prevention=prevention, mitigation=mitigation,
-                        controlled_likelihood=controlled_likelihood, controlled_severity=controlled_severity, controlled_risk=controlled_risk,
-                        prevention_prompt_expected_output='prevention', mitigation_prompt_expected_output='mitigation')
-    
-    if RA.get_input_check_feedback_message() != '':
-        return Result(is_correct=False, feedback= RA.get_input_check_feedback_message())
-    
-    else:
-        LLM = OpenAILLM()
-
-        question_titles = RA.get_list_of_question_titles()
-        questions = RA.get_list_of_questions()
-        prompts = RA.get_list_of_prompts()
-        prompt_outputs = RA.get_list_of_prompt_outputs(LLM)
-        regex_matches = RA.get_list_of_regex_matches(prompt_outputs)
-        shortform_feedbacks = RA.get_list_of_shortform_feedback_from_regex_matches(regex_matches)
-        is_everything_correct = RA.are_all_prompt_outputs_correct(prompt_outputs) and RA.are_all_multiplications_correct()
-        booleans_indicating_which_prompts_need_feedback = RA.get_booleans_indicating_which_prompts_need_feedback(regex_matches)
-
-        feedback = f'''
-        ------ FEEDBACK ------\n\n
-        '''
-
-        for i in range(len(prompts)):
-            question_title = question_titles[i]
-            prompt_output = prompt_outputs[i]
-            shortform_feedback = shortform_feedbacks[i]
-
-            feedback += f'--- Q{i + 1}: {question_title} ---\n\n'
-            feedback += f'Feedback {i + 1}: {shortform_feedback}\n\n'
-            if booleans_indicating_which_prompts_need_feedback[i] == True:
-                feedback += f'Explanation {i + 1}: {prompt_output}\n\n\n'
-
-        feedback += f'--- Controlled risk multiplication is: {RA.check_controlled_risk()} ---\n\n'
-        feedback += f'--- Uncontrolled risk multiplication is: {RA.check_uncontrolled_risk()} ---\n\n'
-
-        return Result(is_correct=is_everything_correct, feedback=feedback)
     
 import streamlit as st
-# from evaluation import evaluation_function
+from evaluation import evaluation_function
 
 with st.form('risk_assessment'):
     activity = st.text_input('Activity', value='Fluids laboratory')
